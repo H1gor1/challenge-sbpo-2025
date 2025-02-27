9f4d24cc033f0e57ef4595d8b705f5ff1e7ab0eb
Vitor Rodrigues <vitorrodrys@gmail.com>
2025-02-27 19:22:57 -0300

    Update BDecoder to include instanceData on your interface

--- src/main/java/org/sbpo2025/challenge/brkga_decoders/BDecoder.java
+++ src/main/java/org/sbpo2025/challenge/brkga_decoders/BDecoder.java
@@ -7,7 +7,8 @@ interface BDecoder {
      * Receives an array of random keys and decodes them into an instance of ChallengeSolution.
      * 
      * @param keys An array of random keys, each in the range [0, 1].
+     * @param instanceData The data of the current problem instance that was provided to solve it.
      * @return An instance of ChallengeSolution representing a valid solution to the problem.
      */
-    abstract ChallengeSolution decode(double[] keys);
+    abstract ChallengeSolution decode(double[] keys, ProblemData instanceData);
 }
