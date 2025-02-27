90a1118bd282917034ba2a632bdba1210e953da4
Vitor Rodrigues <vitorrodrys@gmail.com>
2025-02-27 19:22:39 -0300

    Add class to storage instance data

---
+++ src/main/java/org/sbpo2025/challenge/ProblemData.java
@@ -0,0 +1,14 @@
+package org.sbpo2025.challenge;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public record ProblemData(
+    List<Map<Integer, Integer>> orders,
+    List<Map<Integer, Integer>> aisles,
+    int nItems,
+    int waveSizeLB,
+    int waveSizeUB
+){}
