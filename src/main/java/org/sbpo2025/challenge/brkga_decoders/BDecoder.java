74200289b66a5961146aad6f3cd8b73caf2e70e7
Vitor Rodrigues <vitorrodrys@gmail.com>
2025-02-28 10:23:25 -0300

    Using list of double instead of double[]

--- src/main/java/org/sbpo2025/challenge/brkga_decoders/BDecoder.java
+++ src/main/java/org/sbpo2025/challenge/brkga_decoders/BDecoder.java
@@ -1,5 +1,7 @@
 package org.sbpo2025.challenge.brkga_decoders;
 
+import java.util.List;
+
 import org.sbpo2025.ChallengeSolution;
 
 interface BDecoder {
@@ -10,5 +12,5 @@ interface BDecoder {
      * @param instanceData The data of the current problem instance that was provided to solve it.
      * @return An instance of ChallengeSolution representing a valid solution to the problem.
      */
-    ChallengeSolution decode(double[] keys, ProblemData instanceData);
+    ChallengeSolution decode(List<Double> keys, ProblemData instanceData);
 }
