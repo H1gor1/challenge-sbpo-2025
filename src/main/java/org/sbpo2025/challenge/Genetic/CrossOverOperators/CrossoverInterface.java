df44c2de3324f7a2ff0b1ad99ae228fc0b9d3f9f
Vitor Rodrigues <vitorrodrys@gmail.com>
2025-03-02 18:32:36 -0300

    Add crossover interface

---
+++ src/main/java/org/sbpo2025/challenge/Genetic/CrossOverOperators/CrossoverInterface.java
@@ -0,0 +1,17 @@
+package org.sbpo2025.challenge.Genetic.CrossOverOperators;
+
+import java.util.List;
+
+public interface CrossoverInterface {
+
+    /**
+     * Should make a crossover between two parents, generating a child random key.
+     * Should integrate in the your logic the pBetterParent parameter, that is the prodability of
+     * the gene of the best parent be in the child.
+     * @param bestParent The random keys of the best parent.
+     * @param worstFather The random keys of the worst parent. 
+     * @param pBetterParent The probability of the gene of the best parent be in the child.
+     * @return The random keys of the child.
+     */
+    public List<Double> makeCrossOver(List<Double> bestParent, List<Double> worstFather, Double pBetterParent);
+}
