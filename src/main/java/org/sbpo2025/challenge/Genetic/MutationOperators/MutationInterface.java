5d0431f0184d04f27f0710b021a664b5945ca9d4
Vitor Rodrigues <vitorrodrys@gmail.com>
2025-03-03 10:17:08 -0300

    Simplifiyng mutation interface

--- src/main/java/org/sbpo2025/challenge/Genetic/MutationOperators/MutationInterface.java
+++ src/main/java/org/sbpo2025/challenge/Genetic/MutationOperators/MutationInterface.java
@@ -7,7 +7,6 @@ public interface MutationInterface {
     /**
      * Should make a mutation in the random keys accordance to the mutation rate.
      * @param randomKeys The random keys that should be mutated.
-     * @param mutationRate The mutation rate that should be used to make the mutation.
      */
-    public void makeMutation(List<Double> randomKeys, Double mutationRate);
+    public void makeMutation(List<Double> randomKeys);
 }
