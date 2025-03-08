343b28879c08132c9efa4f1636e16a292855208f
Vitor Rodrigues <vitorrodrys@gmail.com>
2025-03-08 20:42:39 -0300

    Add random reset

--- src/main/java/org/sbpo2025/challenge/Genetic/MutationOperators/RandomReset.java
+++ src/main/java/org/sbpo2025/challenge/Genetic/MutationOperators/RandomReset.java
@@ -5,9 +5,18 @@ import java.util.Random;
 
 public class RandomReset implements MutationOp {
 
+    final private int diversityLevel;
+
+    public RandomReset(int diversityLevel) {
+        this.diversityLevel = diversityLevel;
+    }
+
     @Override
     public void makeMutation(List<Double> randomKeys, Random random) {
-        int target = random.nextInt(randomKeys.size());
-        randomKeys.set(target, random.nextDouble());
+        int target;
+        for (int i = 0; i < diversityLevel; i++) {
+            target = random.nextInt(randomKeys.size());
+            randomKeys.set(target, random.nextDouble());
+        }
     }
 }
