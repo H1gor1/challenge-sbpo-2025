ecf60408cadec7c6e8e75bd1e1126919c4fb225e
Vitor <vitorrodrys@gmail.com>
2025-03-16 16:07:58 -0300

    Fix: rename worstFather to worstParent in crossover methods

--- src/main/java/org/sbpo2025/challenge/Genetic/CrossOverOperators/UniformCrossOver.java
+++ src/main/java/org/sbpo2025/challenge/Genetic/CrossOverOperators/UniformCrossOver.java
@@ -8,7 +8,7 @@ import java.util.Random;
 public class UniformCrossOver implements CrossOverOp{
 
     @Override
-    public List<Double> makeCrossOver(List<Double> bestParent, List<Double> worstFather, Double pBetterParent, Random random) {
+    public List<Double> makeCrossOver(List<Double> bestParent, List<Double> worstParent, Double pBetterParent, Random random) {
         List<Double> child = new ArrayList<>();
         for(int i = 0; i < bestParent.size(); i++){
             if(random.nextDouble() < pBetterParent){
