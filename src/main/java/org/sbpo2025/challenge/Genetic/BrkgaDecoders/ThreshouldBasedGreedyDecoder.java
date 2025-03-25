b368536ef2358689db2d0d57f309514d9b09cb99
Vitor Rodrigues <vitorrodrys@gmail.com>
2025-03-24 23:40:48 -0300

    Simplifying ThresholdBasedDecoder since we no longer use the TripleKeyDecoder.

--- src/main/java/org/sbpo2025/challenge/Genetic/BrkgaDecoders/ThreshouldBasedGreedyDecoder.java
+++ src/main/java/org/sbpo2025/challenge/Genetic/BrkgaDecoders/ThreshouldBasedGreedyDecoder.java
@@ -1,11 +1,14 @@
 package org.sbpo2025.challenge.Genetic.BrkgaDecoders;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 
+import org.sbpo2025.challenge.ChallengeSolution;
 import org.sbpo2025.challenge.ProblemData;
 
-public class ThreshouldBasedGreedyDecoder extends TripleKeyGreedyDecoder {
+public class ThreshouldBasedGreedyDecoder extends Decoder {
 
     @Override
     public int getRKeysSize(ProblemData instanceData) {
@@ -37,5 +40,70 @@ public class ThreshouldBasedGreedyDecoder extends TripleKeyGreedyDecoder {
         }
         return List.of(orderIndexes, aisleIndexes, List.of(aisleIndexes.size()));
 
+    }
+
+    private void countIntialItems(
+        int[] quantItens,
+        List<Integer> aisleIndexes,
+        int QAisles,
+        ProblemData instanceData,
+        HashSet<Integer> aisleResp
+    ) {
+        List<Map<Integer, Integer>> aisles = instanceData.aisles();
+
+        for (int i = 0; i < QAisles; i++) {
+            int aisleIndex = aisleIndexes.get(i);
+            Map<Integer, Integer> aisle = aisles.get(aisleIndex);
+
+            for (Map.Entry<Integer, Integer> kv : aisle.entrySet()) {
+                quantItens[kv.getKey()] += kv.getValue();
+            }
+
+            aisleResp.add(aisleIndex);
+        }
+    }
+
+    @Override
+    protected ChallengeSolution performDecode(List<List<Integer>> evaluationOrder, ProblemData instanceData) {
+        List<Integer> orderIndexes = evaluationOrder.get(0);
+        List<Integer> aisleIndexes = evaluationOrder.get(1);
+        List<Integer> qAisleIndexes = evaluationOrder.get(2);
+        Integer QAisles = qAisleIndexes.get(0);
+
+        HashSet<Integer> orderResp = new HashSet<>();
+        HashSet<Integer> aisleResp = new HashSet<>();
+
+        int[] QuantItens = new int[instanceData.nItems()];
+        countIntialItems(QuantItens, aisleIndexes, QAisles, instanceData, aisleResp);
+        
+        int itensSum = 0;
+        int waveSizeUB = instanceData.waveSizeUB();
+        int waveSizeLB = instanceData.waveSizeLB();
+        
+        for (Integer order : orderIndexes) {
+            if (!isOrderServable(order, QuantItens, instanceData)) {
+                continue;
+            }
+            int orderItens = sumOrderItems(order, instanceData);
+
+            if (itensSum + orderItens > waveSizeUB) {
+                continue;
+            }
+            itensSum += orderItens;
+            updateQuantItens(order, QuantItens, instanceData);
+            orderResp.add(order);
+        }
+
+        double fo = (itensSum < waveSizeLB) ? 0.0 : (itensSum / (double) QAisles);
+
+        return new ChallengeSolution(orderResp, aisleResp, fo, itensSum);
+    }
+
+    private int sumOrderItems(Integer order, ProblemData instanceData) {
+        int sum = 0;
+        for (int qty : instanceData.orders().get(order).values()) {
+            sum += qty;
+        }
+        return sum;
     }
 }
