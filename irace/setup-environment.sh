44a50f849310164160b0947921e253a89342aca2
Vitor Rodrigues <vitorrodrys@gmail.com>
2025-08-28 09:56:46 -0300

    feat: add script to setup environment automatically

---
+++ irace/setup-environment.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if ! (grep -qi "ID_LIKE=.*debian" /etc/os-release || grep -qi "^ID=debian" /etc/os-release); then
+    echo "This script is intended to run on a Debian-based distro."
+    exit 1
+fi
+
+python3 -m venv .venv
+source .venv/bin/activate
+pip install -r requirements.txt
+
+sudo apt update
+sudo apt install r-base -y
