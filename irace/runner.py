e9e5e32b0aa10f0448f6bd8535b63bea53be0183
Vitor Rodrigues <vitorrodrys@gmail.com>
2025-08-30 23:24:49 -0300

    fix: irace runner

--- irace/runner.py
+++ irace/runner.py
@@ -4,6 +4,8 @@ from textwrap import dedent
 
 PROJECT_DIR = pathlib.Path("../")
 SCENARIO_FILE = pathlib.Path("scenario.txt")
+OLD_ENV_FILE = pathlib.Path(".env")
+
 
 def compile_code(source_folder) -> None:
     subprocess.run(
@@ -16,19 +18,23 @@ def compile_code(source_folder) -> None:
 
 
 def main():
+    if OLD_ENV_FILE.exists():
+        OLD_ENV_FILE.unlink()  # remove the old env file to avoid interfering with the tests
+
     compile_code(PROJECT_DIR)
     print("calling Irace parameter optimizer")
     r_command = dedent(
         f"""
         library(irace)
         scenario <- irace::readScenario("{SCENARIO_FILE.resolve()}")
-        irace::irace(scenario)
+        irace_result <- irace::irace(scenario)
+
+        best <- irace::getFinalElites(irace_result, n = 1)[[1]]
+        env_lines <- paste(names(best), best, sep="=")
+        writeLines(env_lines, ".env")
         """
     )
-    subprocess.run(
-        ["Rscript", "-e", r_command],
-        check=True
-    )
+    subprocess.run(["Rscript", "-e", r_command], check=True)
 
 
 main()
